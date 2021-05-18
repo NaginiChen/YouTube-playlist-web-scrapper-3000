cba41e2050f818e7aecb3f005ccee8adae33f8b0
NaginiChen <naginichen@gmail.com>
2021-05-18 12:52:11 -0400

    scrolling added, still a little buggy

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,18 @@ const puppeteer = require('puppeteer');
 const express = require('express');
 const router = express.Router();
 
-async function scrapeProduct(url) {
+const scrapeProduct = async(url) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
+
+    // await page.setViewport({
+    //     width: 200,
+    //     height: 800
+    // });
+
+    page.on('console', consoleObj => console.log(consoleObj.text()))
+    await autoScroll(page)
 
     const rawText = await page.evaluate(() => {
 
@@ -22,6 +30,32 @@ async function scrapeProduct(url) {
 
     await browser.close();
     return rawText;
+}
+
+const autoScroll = async(page) => {
+    await page.evaluate(async() => {
+        await new Promise((resolve, reject) => {
+            let totalHeight = 0
+            const distance = 1000
+            const timer = setInterval(() => {
+                const scrollHeight = document.documentElement.scrollHeight
+                console.log('scroll height: ', scrollHeight)
+
+                const oldPosition = document.documentElement.scrollTop
+                window.scrollBy(0, distance);
+                console.log('position: ', document.documentElement.scrollTop)
+                console.log(`total height: ${totalHeight} \n`)
+                const newPosition = document.documentElement.scrollTop
+
+                totalHeight += distance;
+
+                if(oldPosition !== newPosition && totalHeight >= scrollHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 1500);
+        });
+    })
 }
 
 router.get('/', function (req, res) {
